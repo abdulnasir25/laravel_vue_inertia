51606806fa834a62c54d3a17b162e823473a7b1d
Nasir Shah <abdulnasirshah25@gmail.com>
2025-02-11 17:39:49 +0500

    For different pages, different inertia routes approach has been used.

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps({
+        name: String
+    });
+</script>
+
+<template>
+    <div>
+        <h1 class="bg-red-500">About Page - {{ name }}</h1>
+    </div>
+</template>
---
+++ resources/js/Pages/Contact.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps({
+        name: String
+    });
+</script>
+
+<template>
+    <div>
+        <h1>Contact with - {{ name }}</h1>
+    </div>
+</template>
