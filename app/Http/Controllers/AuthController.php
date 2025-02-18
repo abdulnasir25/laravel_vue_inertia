4bd774aaf975863efb237643b8cabdb1160b5f7e
Nasir Shah <abdulnasirshah25@gmail.com>
2025-02-18 10:34:49 +0500

    Flash message added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('dashboard');
+        return redirect()->route('dashboard')->with('success','Welcome to the family!');
     }
 
     public function login(Request $request)
